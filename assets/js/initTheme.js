1b211e5dcb25b34703aee1cce6007ad5706021cd
Yusup Apandi <yusup18apandi@gmail.com>
2022-10-06 06:34:36 +0700

    first commit

---
+++ assets/js/initTheme.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./src/assets/js/initTheme.js ***!
+  \************************************/
+var body = document.body;
+
+if (localStorage.getItem('theme') == 'theme-dark') {
+  body.classList.add("theme-dark");
+} else {
+  body.classList.add("theme-light");
+}
+
+;
+/******/ })()
+;
